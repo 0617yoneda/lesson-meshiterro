7de5f215a2c02df67bfd0af9623ceb4099151ac9
EC2 Default User <ec2-user@ip-172-31-74-120.ec2.internal>
2020-11-30 11:40:58 +0000

    [add]投稿画面詳細、一覧、新規投稿画面作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'db3d9d7c464d87f22d066f56c420697bb1632db45fd85ef1af676f4c1af496395ab1e84753d25778f6d8abb14c5201d53e2752dfd0f89d0abcc19cc49a14447c'
